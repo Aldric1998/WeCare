2a301c6621ed8e64489d82891ee71b76a21739c5
Nicolas Dubois <duboisnicolas98@gmail.com>
2021-11-23 16:41:26 +0100

    Specialist a lot

--- app/controllers/specialists_controller.rb
+++ app/controllers/specialists_controller.rb
@@ -1,2 +1,61 @@
 class SpecialistsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show, :index]
+  def index
+    if params[:query].present?
+      sql_query = " \
+        specialists.first_name @@ :query \
+        OR specialists.last_name @@ :query \
+        OR specialists.address @@ :query \
+        OR users.first_name @@ :query \
+        OR users.last_name @@ :query \
+      "
+      @specialists = Specialist.joins(:user).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @specialists = Specialist.all
+    end
+
+    # @markers = @specialists.geocoded.map do |specialist|
+    #   {
+    #     lat: specialist.latitude,
+    #     lng: specialist.longitude,
+    #     info_window: render_to_string(partial: "info_window", locals: { specialist: specialist }),
+    #   }
+    # end
+  end
+
+  def new
+    @specialist = Specialist.new
+  end
+
+  def create
+    @specialist = Specialist.new(specialist_params)
+    @specialist.user = current_user
+    @specialist.availability = true
+    @specialist.save
+    redirect_to specialist_path(@specialist)
+  end
+
+  def show
+    @specialist = Specialist.find(params[:id])
+    @consultations = @specialist.consultations.where(user: current_user)
+    @markers = @specialist.geocode
+    @markers = [{
+        lat: @specialist.latitude,
+        lng: @specialist.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { specialist: @specialist }),
+      }]
+  end
+
+  def destroy
+    @specialist = Specialist.find(params[:id])
+    @specialist.destroy
+    redirect_to specialists_path
+  end
+
+  private
+
+  def specialist_params
+    params.require(:specialist).permit(:first_name, :last_name, :speciality_id, :address, :phonenumber, :email, :availability, :picture)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_141000) do
+ActiveRecord::Schema.define(version: 2021_11_23_100330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,14 @@ ActiveRecord::Schema.define(version: 2021_11_22_141000) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["first_name"], name: "index_users_on_first_name"
+    t.index ["last_name"], name: "index_users_on_last_name"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "answers", "possible_answers"
