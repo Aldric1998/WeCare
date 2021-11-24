4fbac1734447b233f8d2727ad1a04a1dddf5a251
Nicolas Dubois <duboisnicolas98@gmail.com>
2021-11-24 14:23:16 +0100

    Specialist very advanced

--- app/models/specialist.rb
+++ app/models/specialist.rb
@@ -5,13 +5,13 @@ class Specialist < ApplicationRecord
 
   # include PgSearch::Model
   # pg_search_scope :global_search,
-    # against: [ :first_name, :last_name, :address ],
-    # associated_against: {
-      # user: [ :first_name, :last_name ]
-    # },
-    # using: {
-      # tsearch: { prefix: true }
-    # }
+  #   against: [ :first_name, :last_name, :address ],
+  #   associated_against: {
+  #     user: [ :first_name, :last_name ]
+  #   },
+  #   using: {
+  #     tsearch: { prefix: true }
+  #   }
   # geocoded_by :address
   # after_validation :geocode, if: :will_save_change_to_address?
 end
