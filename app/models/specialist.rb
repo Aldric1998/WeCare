eeb15924d1b6037d3205b21df5eebda669615a0c
Nicolas Dubois <duboisnicolas98@gmail.com>
2021-11-24 17:20:19 +0100

    Specialist before Arthur

--- app/models/specialist.rb
+++ app/models/specialist.rb
@@ -10,10 +10,10 @@ class Specialist < ApplicationRecord
   #   against: [ :first_name, :last_name, :address ],
   #   associated_against: {
   #     user: [ :first_name, :last_name ]
-  #   },
-  #   using: {
-  #     tsearch: { prefix: true }
-  #   }
+  # },
+  # using: {
+  #   tsearch: { prefix: true }
+  # }
   # geocoded_by :address
   # after_validation :geocode, if: :will_save_change_to_address?
 end
