bd23c7e4dff7077b77daf4f20fe617339434d8cb
Aldric De Gryse <aldricdg@gmail.com>
2021-11-26 12:08:02 +0100

    pushing js

--- app/javascript/controllers/btn_firstaidkit_controller.js
+++ app/javascript/controllers/btn_firstaidkit_controller.js
@@ -2,17 +2,41 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
 
-  static targets = ["content"]
+  static targets = ["content", "content2", "content3", "toggledbtn", "toggledbtn2", "toggledbtn3"]
 
-  // connect() {
-  //   // this.contentTarget.classList.remove("d-none")
-  //   console.log("hello")
-  // }
+  connect() {
+    // this.contentTarget.classList.remove("d-none")
+    console.log("hello")
+  }
 
   clickbtn(event) {
     // this.contentTarget.classList.remove("d-none")
-    console.log(event, this.contentTarget)
+    // console.log(event.currentTarget.id, this.contentTarget)
+    if (event.currentTarget.id == "btn1") {
+      this.contentTarget.classList.toggle("d-none")
+      this.toggledbtnTarget.classList.add("toggled-btn")
+      this.toggledbtn2Target.classList.remove("toggled-btn")
+      this.toggledbtn3Target.classList.remove("toggled-btn")
+      this.content2Target.classList.add("d-none")
+      this.content3Target.classList.add("d-none")
+    }
+    else if (event.currentTarget.id == "btn2") {
+      this.content2Target.classList.toggle("d-none")
+      this.toggledbtn2Target.classList.add("toggled-btn")
+      this.toggledbtnTarget.classList.remove("toggled-btn")
+      this.toggledbtn3Target.classList.remove("toggled-btn")
+      this.contentTarget.classList.add("d-none")
+      this.content3Target.classList.add("d-none")
+    }
 
+    else if (event.currentTarget.id == "btn3") {
+      this.content3Target.classList.toggle("d-none")
+      this.toggledbtn3Target.classList.add("toggled-btn")
+      this.toggledbtnTarget.classList.remove("toggled-btn")
+      this.toggledbtn2Target.classList.remove("toggled-btn")
+      this.contentTarget.classList.add("d-none")
+      this.content2Target.classList.add("d-none")
+    }
   }
 
 }
