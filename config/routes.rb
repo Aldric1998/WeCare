77bbbca14df6461e31ab830bddbcc20d5108fb59
Arthur van der Elst <arthurvde@hotmail.fr>
2021-11-30 17:33:23 +0100

    push tout ca

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,9 @@ Rails.application.routes.draw do
   resources :answers, only: %i[index show create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :specialists, only: %i[index show new create destroy] do
-    resources :consultations, only: %i[new create destroy]
+    resources :consultations, only: %i[new create]
   end
-
+  resources :consultations, only: :destroy
   resources :statements, only: %i[index]
 
 end
