469cd71b64d59170b6966aad2270d6361b39fc1e
Nicolas Dubois <duboisnicolas98@gmail.com>
2021-12-01 14:33:54 +0100

    True flatpickr

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,17 +1,34 @@
 import flatpickr from "flatpickr";
 
+function formatDate() {
+  var d = new Date(),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2)
+    month = '0' + month;
+  if (day.length < 2)
+    day = '0' + day;
+
+  return [year, month, day].join('-');
+}
+
 const initFlatpickr = () => {
+  let inputs = document.querySelectorAll(".datepicker")
+  let disabled_dates = JSON.parse(inputs[0].dataset.disabledDateStart).map((input, i) => {
+    return {
+      from: JSON.parse(inputs[0].dataset.disabledDateStart)[i],
+      to: JSON.parse(inputs[0].dataset.disabledDateEnd)[i]
+    }
+  })
+  console.log(disabled_dates)
   flatpickr(".datepicker", {
-    enableTime: true,
+      dateFormat: "Y-m-d",
+      disable: disabled_dates,
+      minDate: "today"
 
-    // disable: [
-    //     {
-    //         from: "2020-04-01",
-    //         to: date.setDate(date.getDate() - 1)
-    //     },
-
-    // ]
-  });
+    });
 }
 
 
